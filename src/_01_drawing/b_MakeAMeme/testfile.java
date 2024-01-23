eeb1f837678b5c7a93c987f67229fc66dc7c3992
league <league@imac-34.attlocal.net>
2024-01-22 17:04:32 -0800

    this is a test

---
+++ src/_01_drawing/b_MakeAMeme/testfile.java
@@ -0,0 +1,41 @@
+package _01_drawing.b_MakeAMeme;
+
+public class testfile {
+
+}
+
+//
+//1. Drag and drop the fistPump.png image onto this sketch
+/////Users/league/git/level-0-module-0-pickleguy65/src/_01_drawing/b_MakeAMeme/fistPump.png
+//void setup() {
+//// 2. Set the size of the sketch to be 500 x 500
+//size (500,500);
+//// The following code loads the image into the program
+//PImage baby = loadImage("fistPump.png");
+//baby.resize(500,500);
+//// 3. Resize the baby image to be the same size as the sketch
+////    Use the baby.resize() command for this
+//
+//
+//// 4. Set the baby image to be the background of your sketch
+////    Use the background() command for this
+//background(baby);
+//
+//// 5. Write the message "TESTED MY PROGRAM" at the top of the meme
+////    Use the text() command for this
+//;
+//;}
+//// 6. Now make the message bigger so that it fills the width of the sketch
+////    Use the textSize() command for this and put it BEFORE you write the text!
+//
+//void draw() {  
+//textSize(40);
+//text("when i get to go to sleep", 15,50); 
+
+
+// 7. Write the messsage "IT WORKED FIRST TIME" at the bottom of the meme
+
+
+// 8. Change the color of the text. Use the fill() command for this
+//    Put it BEFORE you write the text!
+
