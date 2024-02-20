8753c875a27e71c049486b3084d2dc432f15ba04
league <league@localhost>
2024-02-19 16:57:53 -0800

    robot drawings

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -12,24 +12,28 @@ public class PentagonCrazy {
 	public static void main(String[] args) {
 		
 		//  Create a new Robot
-
+		Robot rob = new Robot();
 		//  Put the robot's pen down
-
+		rob.penDown();
 		//  SPEED. Make the robot go at maximum speed (100)
-
+    	rob.setSpeed(100);
 		//  COLOR. Set the pen to a color that you like for the shape
 
 		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape will have.
 		//  				(Hint: its called PentagonCrazy)
-
+        int sides = 5;
 		//  TURN ANGLE. Make another int variable for the angle the robot must turn. 
 		//  			Hint: Divide 360 by the number of sides the shape has to get the angle.
-		
+		int loopRepeated =360/5;
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-
+    	int count= 0;
 		//  LOOP. Start a while loop to repeat the DRAW, TURN, and INCREASE COUNT code 200 times. 
-		
+    	while ( count < 4 )  {
+    		count +=1;
+    		rob.move(count);
+    		rob.turn(loopRepeated);
+    	}
 				//  DRAW.  Make the robot move the amount in your count variable
 		
 				//  TURN.  Turn the robot the amount in your angle variable + 1
